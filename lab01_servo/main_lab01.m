ad1c233ecd61f78b3e6c249911db5c5b9e6e2875
bezkor <13736343+bezkor@users.noreply.github.com>
2023-02-12 22:28:42 +0200

    Fix Lab01

--- lab01_servo/main_lab01.m
+++ lab01_servo/main_lab01.m
@@ -8,6 +8,14 @@ else
   disp('Running in Matlab');
 end;
 
+% ========== START EDIT ==================
+% Значення налаштувань PID регулятора
+KP=6
+KI=1
+KD=5
+% ========== STOP EDIT ===================
+
+
 % Характеристики двигуна постійного струму
 J = 0.01; % Момент інерції
 b = 0.1;  % Момент тертя
@@ -19,47 +27,23 @@ L = 0.5;  % Індуктивність якоря
 s=tf('s')
 DC_motor = K/(s*((J*s+b)*(L*s+R)+K^2))
 
-
 % Передатна функція PID регулятора
 TF_pid=@(Kpid) pid(Kpid(1),Kpid(2),Kpid(3));
 
-% Передатні функція сервоприводу
+% Передатна функція сервоприводу
 TF_servo=@(Kpid) feedback(DC_motor*TF_pid(Kpid),1);
+% Передатна функція помилки сервоприводу
 TF_error=@(Kpid) feedback(1,DC_motor*TF_pid(Kpid));
+% Передатна функція помилки сервоприводу по збуренню
 TF_disturbance=@(Kpid) feedback(DC_motor,TF_pid(Kpid));
-
-% Значення налаштувань PID регулятора
-KP=6
-KI=1
-KD=5
-
-% Функція оптимізації
-qscore=@(Kpid) norm(TF_error(Kpid));
-
-% Пошук оптимального рішення
-options = optimset('TolX',1e-6,'Display','iter','TolFun',1e-6);
-Opt_pid=fminsearch(qscore,[KP,KI,KD],options)
 
 % Передатна функція сервоприводу з ручними налаштуваннями
 WservoM=TF_servo([KP,KI,KD])
+% Передатна функція реакції на зовнішнє збурення
 WdistM=TF_disturbance([KP,KI,KD])
-% Передатна функція сервоприводу з оптимізованими налаштуваннями
-Wservo=TF_servo(Opt_pid)
-Wdist=TF_disturbance(Opt_pid)
+
 % Порівняння перехідних процесів
 figure(1)
-step(WservoM,Wservo);
-figure(2)
-step(WdistM,Wdist);
+step(WservoM,WdistM, 15);
 
 
-% Порівняння показників якості
-fprintf("Ручні налаштування :[ ");
-fprintf("%g ",[KP,KI,KD]);
-fprintf("]; ");
-fprintf("Q = %f\n",qscore([KP,KI,KD]));
-
-fprintf("Оптимізовані налаштування :[ ");
-fprintf("%g ",Opt_pid);
-fprintf("]; ");
-fprintf("Q = %f\n",qscore(Opt_pid));
